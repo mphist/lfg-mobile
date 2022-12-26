0e195f0dc1d44bb42602c2934624c900cdef9a75
mphist <mphist@gmail.com>
2022-12-26 15:18:22 -0500

    Tab bar icons

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Provider as PaperProvider } from 'react-native-paper'
+import MaterialCommunityiIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import TopicsTabView from './src/components/TopicsTabView/TopicsTabView'
 import TestView from './src/components/TestView'
@@ -24,16 +25,29 @@ export default function App() {
             headerTransparent: true,
             headerTintColor: 'black',
             headerTitleAlign: 'center',
+            tabBarStyle: {
+              // backgroundColor: 'black',
+            },
           }}
         >
           <Tab.Screen
             name='Topics'
             component={TopicsTabView}
-            // options={{
-            //   headerTintColor: 'white',
-            // }}
+            options={{
+              tabBarIcon: () => (
+                <MaterialCommunityiIcons name='view-headline' size={20} />
+              ),
+            }}
           />
-          <Tab.Screen name='testView' component={TestView} />
+          <Tab.Screen
+            name='Groups'
+            component={TestView}
+            options={{
+              tabBarIcon: () => (
+                <MaterialCommunityiIcons name='account-multiple' size={20} />
+              ),
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </PaperProvider>
